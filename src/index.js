ac0116e10c1d3b06f3e93fb6152e9a1ee577f8ff
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-16 14:54:21 +0530

    new components has been added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1> Hello World </h1>
+    <Hello greeting={`hello react ninja`}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
