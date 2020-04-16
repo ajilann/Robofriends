dba2ee33b07e060da0a8e366334403899be8b247
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-16 18:29:47 +0530

    added dynamic data to the cards

--- src/Cards.js
+++ src/Cards.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Cards = () => {
+const Cards = ({id, name, email}) => {
+	//const {id, name, email} = props;
 return (
 
-	<div className = 'bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' > 
-	<img alt = "Robots" src="https://robohash.org/test?200x200" />
+	<div className = ' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' > 
+	<img alt = "Robots" src= {`https://robohash.org/${id}?size=200x200`} />
 	<div> 
-	<h2> Jane Doe </h2>
-	 <p> jane@email.com</p>
+	<h2> {name} </h2>
+	 <p> {email}</p>
 	</div>
 	</div>
 	);
---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+{
+id: 1,
+name: 'james carlos',
+username: 'jamcar',
+email: 'jamcar@easy4.com'
+},
+{
+id: 2,
+name: 'margrath mar',
+username: 'mgrth',
+email: 'mgrth@tdleb.com'
+},
+{
+id: 3,
+name: 'susan lady',
+username: 'susla',
+email: 'susla@tnd.com'
+},
+{
+id: 4,
+name: 'reper john',
+username: 'repjo',
+email: 'repjo@easy4.com'
+},
+{
+id: 5,
+name: 'jamal suby',
+username: 'jamsu',
+email: 'jamsu@tnd.com'
+},
+{
+id: 6,
+name: 'sharukh khan',
+username: 'shakh',
+email: 'shak@redbull.com'
+},
+{
+id: 7,
+name: 'john smith',
+username: 'johsm',
+email: 'johsm@ligter.com'
+},
+{
+id: 8,
+name: 'sakariya lobes',
+username: 'saklob',
+email: 'saklob@easy4.com'
+},
+]
