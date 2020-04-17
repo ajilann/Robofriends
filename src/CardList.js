84ef1cd8b8b60d9f092b9ef990aabda95263ac33
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-17 14:25:03 +0530

    the cards are looped for adding long data dynamically

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Cards from './Cards';
 
 const CardList = ({robots}) => {
+	/* const cardArray = */
 	return (
-	<div>
-		<Cards id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email}/>
-    	<Cards id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
-    	<Cards id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
-    </div>
+	<div> 
+	{
+		robots.map((user, i) => {
+		return (
+			<Cards key= {i} 
+			id={robots[i].id} 
+			name={robots[i].name} 
+			username={robots[i].username} 
+			email={robots[i].email} 
+			/> 
+			);
+		})
+	}
+	</div>
 );
 }
 export default CardList;
