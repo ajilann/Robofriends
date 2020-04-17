41d9487d08f62436d33ca81ed5edf4a9bf157518
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-17 17:15:58 +0530

    new searchbox component is added. The state is created to change values by connecting robots and search fields through the app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
+ 
+	class App extends Component {
+		constructor() {
+			super()
+			this.state = 
+			{
+ 				robots: robots,
+ 				searchfields: ''
+			}
+	    }
 
-const App = () => {
+	render() {
 	return (
 	<div className='tc'>
 		<h1> RoboFriends </h1>
-		<input type='search' placeholder='search robots' />
-		<CardList robots = {robots} />
+		<SearchBox />
+		<CardList robots = {this.state.robots} />
 	</div>
-);} 
+);
+}
+} 
 export default App;
