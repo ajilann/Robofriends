9c3a709c4b275834521ebb5da8895ea23066a2de
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-18 20:53:21 +0530

    if the site not rendered quickly then display loading

--- src/App.js
+++ src/App.js
@@ -34,13 +34,17 @@ import './App.css'
 		 const filteredRobots = this.state.robots.filter(robots =>{
 	   	return robots.name.toLowerCase().includes(this.state.searchfields.toLowerCase());
 	   })
+		 if (this.state.robots.length === 0){
+		 	return <h1>Loading </h1>
+		 }
+		 else {
 	return (
 	<div className='tc'>
 		<h1 className= 'f1'> RoboFriends </h1>
 		<SearchBox searchChange = {this.onSearchChange}/>  
 		<CardList robots = {filteredRobots} />
 	</div>
-);
+);}
 }
 }
 
