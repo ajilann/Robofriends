6e0191fc420d5fbc185d7777c8d9dcf451cb612e
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-19 14:51:29 +0530

    A scroll component is created for adding styles and wrap to another component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 //import { robots } from './robots';
 import SearchBox from './SearchBox';
-import './App.css'
+import './App.css';
+import Scroll from './Scroll'
+
 	class App extends Component {
 		constructor() {
 			super()
@@ -41,8 +43,10 @@ import './App.css'
 	return (
 	<div className='tc'>
 		<h1 className= 'f1'> RoboFriends </h1>
-		<SearchBox searchChange = {this.onSearchChange}/>  
+		<SearchBox searchChange = {this.onSearchChange}/>
+		<Scroll>  
 		<CardList robots = {filteredRobots} />
+		</Scroll>
 	</div>
 );}
 }
