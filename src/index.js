1aa04db0609127591a25ac7f786b21b005a943cf
ajilann <ajilan.nazarudeen@gmail.com>
2020-04-21 17:30:27 +0530

    actions and constants files been added to take place the redux environment for the app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 //import Cards from './Cards';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 //import { robots } from './robots';
 //import CardList from './CardList';
 
